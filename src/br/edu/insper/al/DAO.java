bb5ea470f59b5aa0482f47811ebf2e828c7a76f1
Arfel <rafael_1999_@hotmail.com.br>
2019-09-04 12:45:54 -0300

    aaa

--- src/br/edu/insper/al/DAO.java
+++ src/br/edu/insper/al/DAO.java
@@ -1,5 +1,6 @@
 package br.edu.insper.al;
 
+import java.sql.Array;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -12,16 +13,51 @@ public class DAO {
 	private Connection connection;
 	
 	public DAO() {
-	 try {
-		Class.forName("com.mysql.cj.jdbc.Driver");
-		connection  = DriverManager.getConnection(
-				"jdbc:mysql://localhost/Boiei", "Arfel", "R4f43!arafle99");
-	} catch (ClassNotFoundException | SQLException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			connection  = DriverManager.getConnection(
+					"jdbc:mysql://localhost/Boiei", "", "");
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
+	public boolean login(User user) {
+		boolean status = false;
+		try {
+			PreparedStatement stmt;
+			String sql = "SELECT * FROM Users WHERE username=? AND password=?";
+
+			stmt = connection.prepareStatement(sql);
+			stmt.setString(1, user.getUsername());
+			stmt.setString(2, user.getPassword());
+			ResultSet res = stmt.executeQuery();
+			status = res.next();
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return status;
 	}
-	
+	public int getUserId(User user) {
+		int userId = 0;
+		try {
+			PreparedStatement stmt;
+			String sql = "SELECT * FROM Users WHERE username=?";
+
+			stmt = connection.prepareStatement(sql);
+			stmt.setString(1, user.getUsername());
+			ResultSet rs = stmt.executeQuery();
+			if(rs.next()) {
+				userId = rs.getInt("id");	
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return userId;
+	}
 	public List<User> getListUsers() {
 		List<User> users = new ArrayList<User>();
 		try {
@@ -46,21 +82,20 @@ public class DAO {
 	}
 	
 	public void addUser(User user) {
-			try {
-				PreparedStatement stmt;
-				String sql = "INSERT INTO Users" + "(username,password) values(?,?)";
+		try {
+			PreparedStatement stmt;
+			String sql = "INSERT INTO Users" + "(username,password) values(?,?)";
 
-				stmt = connection.prepareStatement(sql);
-				stmt.setString(1,user.getUsername());
-				stmt.setString(2,user.getPassword());
-				stmt.execute();
-				stmt.close();
+			stmt = connection.prepareStatement(sql);
+			stmt.setString(1,user.getUsername());
+			stmt.setString(2,user.getPassword());
+			stmt.execute();
+			stmt.close();
 
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
 	}
 	
 	public List<Post> getListPosts() {
