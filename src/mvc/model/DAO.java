25180da8ae6a0d8b1a965a9c697b87ebc7594ecf
lucas leal vale <lucaslealvale01@gmail.com>
2019-09-19 07:51:59 -0300

    deploy2

--- src/mvc/model/DAO.java
+++ src/mvc/model/DAO.java
@@ -10,17 +10,22 @@ import java.util.List;
 
 public class DAO {
 	private Connection connection;
-	
+
 	public DAO() {
 		try {
+			String url = System.getenv("mysql_url");
+			String user = System.getenv("mysql_user");
+			String password = System.getenv("mysql_password");
+			
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			connection  = DriverManager.getConnection(
-					"jdbc:mysql://localhost/Boiei", "Arfel", "R4f43!arafle99");
+			connection = DriverManager.getConnection(url, user, password);
+
 		} catch (ClassNotFoundException | SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
+
 	public boolean login(User user) {
 		boolean status = false;
 		try {
@@ -39,6 +44,7 @@ public class DAO {
 		}
 		return status;
 	}
+
 	public int getUserId(User user) {
 		int userId = 0;
 		try {
@@ -48,8 +54,8 @@ public class DAO {
 			stmt = connection.prepareStatement(sql);
 			stmt.setString(1, user.getUsername());
 			ResultSet rs = stmt.executeQuery();
-			if(rs.next()) {
-				userId = rs.getInt("id");	
+			if (rs.next()) {
+				userId = rs.getInt("id");
 			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
@@ -57,11 +63,11 @@ public class DAO {
 		}
 		return userId;
 	}
+
 	public List<User> getListUsers() {
 		List<User> users = new ArrayList<User>();
 		try {
-			PreparedStatement stmt = connection.
-			 prepareStatement("SELECT * FROM Users");
+			PreparedStatement stmt = connection.prepareStatement("SELECT * FROM Users");
 			ResultSet rs;
 			rs = stmt.executeQuery();
 			while (rs.next()) {
@@ -70,30 +76,30 @@ public class DAO {
 				user.setUsername(rs.getString("username"));
 				user.setPassword(rs.getString("password"));
 				users.add(user);
-			rs.close();
-			stmt.close();
-				}
+				rs.close();
+				stmt.close();
+			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return users;
 	}
-	
+
 	public boolean addUser(User user) {
 		try {
 			PreparedStatement stmt;
 			String sql = "SELECT * FROM Users WHERE username=?";
 
 			stmt = connection.prepareStatement(sql);
-			stmt.setString(1,user.getUsername());
+			stmt.setString(1, user.getUsername());
 			ResultSet rs = stmt.executeQuery();
-			if(!rs.next()) {
+			if (!rs.next()) {
 				sql = "INSERT INTO Users" + "(username,password) values(?,?)";
 
 				stmt = connection.prepareStatement(sql);
-				stmt.setString(1,user.getUsername());
-				stmt.setString(2,user.getPassword());
+				stmt.setString(1, user.getUsername());
+				stmt.setString(2, user.getPassword());
 				stmt.execute();
 				stmt.close();
 				return true;
@@ -102,15 +108,15 @@ public class DAO {
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}	
+		}
 		return false;
 	}
-	
+
 	public void addPost(Post post) {
 		try {
 			PreparedStatement stmt;
 			System.out.println(post.getUserId());
-		
+
 			String sql = "INSERT INTO Posts (user_id, text, materia, prioridade) values(?,?,?,?)";
 //			Date date= new Date(0);
 //			Timestamp ts = new Timestamp(date.getTime());
@@ -120,20 +126,20 @@ public class DAO {
 			stmt.setString(3, post.getMateria());
 //			stmt.setTimestamp(4, ts);
 			stmt.setInt(4, post.getPrioridade());
-			stmt.execute() ;
+			stmt.execute();
 			stmt.close();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}	
+		}
 	}
-	
+
 	public List<Post> getListPosts(User user, String orderBy) {
 		List<Post> posts = new ArrayList<Post>();
 		try {
-			String sql = "SELECT * FROM Posts WHERE user_id=? ORDER BY "+ orderBy;
+			String sql = "SELECT * FROM Posts WHERE user_id=? ORDER BY " + orderBy;
 			PreparedStatement stmt = connection.prepareStatement(sql);
-			stmt.setInt(1,user.getId());
+			stmt.setInt(1, user.getId());
 			ResultSet rs = stmt.executeQuery();
 
 			while (rs.next()) {
@@ -154,7 +160,7 @@ public class DAO {
 		}
 		return posts;
 	}
-	
+
 	public void delPost(Post post) {
 		try {
 			PreparedStatement stmt = connection.prepareStatement("DELETE FROM Posts WHERE id=?");
@@ -166,7 +172,7 @@ public class DAO {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void editPost(Post post) {
 		try {
 			PreparedStatement stmt = connection.prepareStatement("UPDATE Posts SET text=? WHERE id=?");
